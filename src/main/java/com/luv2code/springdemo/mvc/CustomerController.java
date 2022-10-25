d304aed54dc79389912566642a64b90c4fc114de
EH-Son <eunha.son08@gmail.com>
2022-10-25 12:22:30 +0900

    오류 해결 및 CustomValidation 추가
    - dependency 수정
    - 오류는 인텔리제이에서 아파치 빌드 시에 라이브러리가 자동으로 out 폴더에 등록되지 않아 발생
     - 직접 수정 후 문제 해결
    - Custom Validation으로 접두사 확인 기능 추가

--- src/main/java/com/luv2code/springdemo/mvc/CustomerController.java
+++ src/main/java/com/luv2code/springdemo/mvc/CustomerController.java
@@ -1,8 +1,11 @@
 package com.luv2code.springdemo.mvc;
 
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -10,22 +13,31 @@ import javax.validation.Valid;
 
 
 @Controller
-@RequestMapping("/customer")
+@RequestMapping(value = "/customer")
 public class CustomerController {
 
-    @RequestMapping("/showForm")
+    @InitBinder
+    public void initBinder(WebDataBinder dataBinder) {
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+
+        dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+    }
+
+    @RequestMapping(value = "/showForm")
     public String showForm(Model model) {
         model.addAttribute("customer", new Customer());
 
         return "customer-form";
     }
 
-    @RequestMapping(value = "/processForm")
+    @RequestMapping("/processForm")
     public String processForm(
             @Valid @ModelAttribute("customer") Customer customer,
             BindingResult bindingResult) {
         System.out.println("Last name: |" + customer.getLastName() + "|");
-        System.out.println(bindingResult.hasErrors());
+
+        System.out.println("Binding result: " + bindingResult);
+
         if(bindingResult.hasErrors()) {
             return "customer-form";
         } else {
